a993c1d4c647cedbd4087640d5dba1106725ffbd
Adam <snescoder@gmail.com>
2015-03-07 18:37:56 -0500

    Optimize for size and hack-friendliness.
    The Y value does not need to be matched for the number of things being
    refilled anymore.  It's initialized to 0, any item that executes ADC does INY,
    and a non-zero value after all items triggers "run next frame" logic.
    
    As such, we now see "was full" instead of "is now full" as the status, so we
    don't have to store the extra flag in `$05F9`, saving a few bytes.  `inc_item`
    didn't even expand to give us this more-convenient info.

--- refill-before-save.xkas-06.asm
+++ refill-before-save.xkas-06.asm
@@ -3,17 +3,15 @@
 
 ; Makes save points refill Samus' energy and supplies, like the ship.
 ;
-; Compared to v1, it's smoothly animated, and hijacks differently...
-; so there are no longer separate versions for the vanilla ROM and for
-; Scyzer's load/save patch.
+; Hijacks the save station PLM and uses some free space in that bank ($84).
 
 lorom
 
 ; hijack point: reconfigure the save-station PLM instructions. do not edit.
-; the normal instructions go something like:
+; the original instructions read:
 ; [8CF1=prompt][B008][B00E=freeze/pose Samus][8C07,2E=saving sound]...
 ; we overwrite those first 3 words and chain into the hijacked instructions,
-; making it act like
+; making it have the effect of:
 ; [B00E][refill_run, repeated until full][8CF1][B008][8C07,2E]...
 org $84AFEE
 	DW refill_start    ; hijack the "save?" message & posing
@@ -22,25 +20,23 @@ org $84AFEE
 	; remaining PLM instructions are used as-is.
 
 ; this can be moved anywhere in bank $84’s free space, and MUST be bank $84.
-; I'm sorry it's so huge--118 ($76) bytes--there's a lot to do.
+; Now optimized to 107 ($6B) bytes--still big, but there's a lot to do.
 org $84FF00
 refill_start:
-	TYA
+	TYA                ; copy pointer to refill_run into A
 	STA $1D27,X        ; set refill_run as next PLM instruction
 	JSR $B00E          ; freeze and pose Samus (saves X/Y itself)
 	STZ $05F9          ; steal "save?" msgbox response RAM for "done?"
 	STZ $0A6A          ; zero "health alarm on" flag
 	LDA #$0001         ; stop sound
-	JSL $80914D        ; sound lib 3 routine
+	JSL $80914D        ; sound lib 3 routine (also saves X/Y)
 
 refill_run:
 	PHX : PHY          ; preserve regs
+	LDY #$0000         ; set up "everything fully refilled" value
 
 	; comment the LDX/JSR line (add a semicolon to the beginning of the line)
-	; of any items you DO NOT want to refill.
-	; for each line you comment, reduce the LDY value by 1: otherwise, the
-	; refill will be infinite.  anything that is full decrements Y.
-	LDY #$0005                ; set up "nothing fully refilled" value
+	; of any items you DO NOT want to refill.  or just delete it.
 
 	LDA #$0005 : STA $12      ; energy increment value per frame
 	LDX #$09C2 : JSR inc_item ; energy tanks
@@ -55,32 +51,25 @@ refill_run:
 	PLY : PLX          ; restore regs
 	ASL                ; set Z flag if A is $0000 (saves 2 bytes vs. CMP)
 	BNE refill_more    ; not all full: run this refill instruction next frame
-	LDA $05F9          ; were we done last frame?
-	BNE hijack_done    ; yes: jump to hijacked code
-	INC                ; no: tell us we were, next frame
-	STA $05F9          ; (so the HUD has a chance to draw at 100%)
+	JMP $8CF1          ; all items full: run hijacked save prompt
+	; (this will stop the current PLM instruction from running again)
 
 refill_more:
-	LDA #$0001         ; run it/us next frame
+	LDA #$0001         ; run this PLM instruction next frame
 	STA $7EDE1C,X      ; write frame delay
 	PLA                ; end current frame's instructions for this PLM
 	RTS                ; run next PLM
 
-hijack_done:
-	JMP $8CF1          ; all items full: run hijacked save prompt
-
-
 inc_item:
 	LDA $0000,X        ; current value
 	CMP $0002,X        ; max value
-	BNE needs_inc      ; full?
-	DEY : RTS          ; yes: mark full and return.
-needs_inc:
-	CLC : ADC $12      ; not full: add current increment value
+	BEQ inc_is_full    ; already full?  just exit out
+	INY                ; not already full: mark as such
+	CLC : ADC $12      ; add current increment value
 	CMP $0002,X        ; is it full now?
 	BCC inc_item_write ; less than full: only save back to current
-	LDA $0002,X        ; equal or overfull: set to full
-	DEY                ; mark full
+	LDA $0002,X        ; equal or overfull: set to full exactly
 inc_item_write:
 	STA $0000,X        ; write new (calculated or max) value to current
+inc_is_full:
 	RTS                ; return
